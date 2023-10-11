0bab307294ba0a0ec14361afb238e484782884a2
linaayson <andersson.lina89@gmail.com>
2023-10-11 11:16:03 +0200

    add required data fix format

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 const apiKey = "d5d4f3f8cd1c728d53bc3cc2ba50620a";
 const lat = 59.333831; // Stockholm
 const lon = 17.980385; // Stockholm
-const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
 
 const container = document.getElementById("weather");
 
@@ -22,10 +22,11 @@ const callApiCurrent = () =>
     })
     .then((data) => {
       // Use specific data from the response to display in the container
-      const temperature = data.main.temp;
+      const temperature = data.main.temp.toFixed(1);
       const description = data.weather[0].description;
+      const name = data.name;
 
-      container.innerText = `Temperature: ${temperature}°C, Weather: ${description}`;
+      container.innerText = `The temperature: ${temperature}°C in ${name} is ${description}.`;
       console.log(data);
     })
     .catch((error) => {
