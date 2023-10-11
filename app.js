14a2db99aa4ead024e89432d36fe7325fc5e7593
linaayson <andersson.lina89@gmail.com>
2023-10-11 14:47:39 +0200

    Co-authored-by: Vardagsbruket <Vardagsbruket@users.noreply.github.com> added html structure and fetching icons

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-//Instructionsfrom OpenWeatherMap on how to make an API call (get lat & lon): https://openweathermap.org/api/one-call-3
-
-// Current weather api
-// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-
-
-
 const apiKey = "d5d4f3f8cd1c728d53bc3cc2ba50620a";
 const lat = 59.333831; // Stockholm
 const lon = 17.980385; // Stockholm
 const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+const container = document.getElementById("container");
 
-const container = document.getElementById("weather");
+// Function to get weather icon URL
+function getWeatherIconURL(iconCode) {
+  return `https://openweathermap.org/img/wn/${iconCode}.png`;
+}
 
 const callApiCurrent = () =>
   fetch(URL)
@@ -24,10 +21,22 @@ const callApiCurrent = () =>
       // Use specific data from the response to display in the container
       const temperature = data.main.temp.toFixed(1);
       const description = data.weather[0].description;
+      const iconCode = data.weather[0].icon; // Icon code from the API
+
+      // Construct the icon URL
+      const iconURL = getWeatherIconURL(iconCode);
+
       const name = data.name;
 
-      container.innerText = `The temperature: ${temperature}°C in ${name} is ${description}.`;
-      console.log(data);
+      container.innerHTML = `
+      <div>
+        <h2 class="temp">${temperature}°C</h2>
+        <h1 class="cityName">${name}</h1>
+        <div class="desc"><p>${description}</p><img src="${iconURL}" 
+        alt="weather icon" class="img-icon"></div>
+      </div>
+        `;
+
     })
     .catch((error) => {
       // Handle errors, such as network issues or invalid responses
@@ -36,6 +45,3 @@ const callApiCurrent = () =>
     });
 
 callApiCurrent();
-//
-
-// const URLGeocoding = `http://api.openweathermap.org/geo/1.0/direct?q=${city name},${state code },${country code }& limit=${ limit }& appid=${apiKey} `;
